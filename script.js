c10db7cfc6755652c60dac1ab2c98fb0ef798a52
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-02-20 19:27:21 +0200

    add variable declarations and naming conventions examples in script.js

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-alert("Hello World");
+// literals
+console.log("amazing"); // string literal
+console.log(40 + 8 + 23 - 10); // number(expression) literal
 
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// variables
+let firstName = "Jonas";
+let age = 23;
+console.log(firstName);
+console.log(age);
 
-console.log(40 + 8 + 23 - 10);
+// naming convention
+/*
+    undefined variable names
+    let 3years = 3; // cannot start with a number
+    let jonas&matilda = 'JM'; // cannot contain special characters
+    let first-name = 'Jonas'; // cannot contain hyphens
+    let new = 27; // reserved keyword
+    let function = 27; // reserved keyword
+    let name = 'Jonas'; // reserved
+    let Name = 'Jonas'; // case sensitive
+*/
+
+// good variable names
+let $function = 27;
+let _function = 27;
+let first_name = "Jonas";
+let firstNamePerson = "Jonas"; // best practice: lower camel case
+let first_name_person = "Jonas";
+let personAge = 27;
+let PI = 3.1415;
+
+// identifiers must be unique and descriptive
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// these identifiers are not unique and descriptive
+let job1 = "Programmer";
+let job2 = "Teacher";
