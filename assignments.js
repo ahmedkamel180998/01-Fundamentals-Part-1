bb3632f0c1fdb46c7efadb582ef515d2d77669c8
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-02-20 19:39:10 +0200

    assignment: values and variables

---
+++ assignments.js
@@ -0,0 +1,11 @@
+let country = "Egypt";
+let continent = "Africa";
+let population = 108000000;
+
+console.log(country);
+console.log(continent);
+
+if (population > 100000000) {
+  let pop = population / 1000000;
+  console.log(pop + " millions");
+}
